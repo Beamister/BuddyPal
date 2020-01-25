01ab3e7903b15eb47a16710935d61ef69edd74c6
Luke Beamish <lukebeamish1@hotmail.co.uk>
2020-01-25 15:40:38 +0000

    Added pubsub publishing

--- tests.py
+++ tests.py
@@ -1,3 +1,7 @@
+from google.cloud import pubsub_v1
+
+topic_name = 'projects/buddypal/topics/BuddyPal'
+
 
 def tests_pass():
     print("Tests passed")
@@ -8,3 +12,7 @@ def tests_fail():
 
 
 tests_fail()
+
+publisher = pubsub_v1.PublisherClient()
+publisher.publish(topic_name, b'My first message!')
+print("Published")
