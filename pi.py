bd10cbf1057d044c9b8b2bbd142d737b2e89cc61
Luke Beamish <lukebeamish1@hotmail.co.uk>
2020-01-25 22:36:33 +0000

    Added pi processing of subscription messages

--- pi.py
+++ pi.py
@@ -1,19 +1,40 @@
+from google.cloud import pubsub_v1
 from gpiozero import LED, Buzzer
 from time import sleep
+import os
 
 ledRed = LED(17)
 ledGreen = LED(18)
 buzzer = Buzzer(27)
 
+subscription_path = 'projects/buddypal/subscriptions/BuddyPal-Sub'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/home/pi/BuddyPal/buddypal-ee7296cbe411.json"
+subscriber = pubsub_v1.SubscriberClient()
+
 buildFailed = False
 
-while buildFailed:
-    ledRed.on()
-    buzzer.on()
-    sleep(1)
-    ledRed.off()
-    buzzer.off()
-    sleep(1)
 
-while not buildFailed:
-    ledGreen.on()
+def check_message(message):
+    global buildFailed
+    message_contents = message.data.decode()
+    if message_contents == 'Passed':
+        buildFailed = False
+    else:
+        buildFailed = True
+    message.ack()
+
+
+future = subscriber.subscribe(subscription_path, check_message)
+
+
+while True:
+    if buildFailed:
+        ledRed.on()
+        buzzer.on()
+        sleep(1)
+        ledRed.off()
+        buzzer.off()
+        sleep(1)
+
+    else:
+        ledGreen.on()
