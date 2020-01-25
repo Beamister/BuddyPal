e729787fb9e603d256c85bc4cc0fce8a838a9e4f
Luke Beamish <lukebeamish1@hotmail.co.uk>
2020-01-25 17:40:44 +0000

    Added pi files

--- tests.py
+++ tests.py
@@ -1,22 +1,20 @@
 from google.cloud import pubsub_v1
 import os
 
-
-print("Path:", os.getcwd())
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/buddy/buddypal/buddypal-118be5edd0f1.json"
 topic_name = 'projects/buddypal/topics/BuddyPal'
 
 
 def tests_pass():
+    publisher = pubsub_v1.PublisherClient()
+    publisher.publish(topic_name, b'Passed')
     print("Tests passed")
 
 
 def tests_fail():
+    publisher = pubsub_v1.PublisherClient()
+    publisher.publish(topic_name, b'Failed')
     print("Tests failed")
 
 
 tests_fail()
-
-publisher = pubsub_v1.PublisherClient()
-publisher.publish(topic_name, b'My first message!')
-print("Published")
